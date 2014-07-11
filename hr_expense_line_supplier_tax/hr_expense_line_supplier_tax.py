5e510a36ea066b1ceb71aa6c6f88c44de75f69df
Mathieu Benoit <mathieu.benoit@savoirfairelinux.com>
2014-07-11 15:41:45 -0400

    Remove dependancy on hr_expense_line_supplier. Add partner field, needed it in dependancy.

--- hr_expense_line_supplier_tax/hr_expense_line_supplier_tax.py
+++ hr_expense_line_supplier_tax/hr_expense_line_supplier_tax.py
@@ -1,44 +1,44 @@
 # -*- encoding: utf-8 -*-
-##############################################################################
-#····
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+# #############################################################################
+#
+# OpenERP, Open Source Management Solution
+# This module copyright (C) 2010 - 2014 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as
+#    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.·····
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
-from osv import osv, fields
-import decimal_precision as dp
+from openerp.osv import fields, orm
 
-class hr_expense_line(osv.osv):
+
+class hr_expense_line(orm.Model):
     _inherit = 'hr.expense.line'
 
-
     def get_tax_id(self, cr, uid, product_id, partner_id, context=None):
-        '''
+        """
             Get the automatic value of the tax id field.
-        '''
-        product     = self.pool.get('product.product').browse(cr, uid, product_id)
-        purch_tax   = product.product_tmpl_id.supplier_taxes_id
+        """
+        product = self.pool.get('product.product').browse(cr, uid, product_id)
+        purch_tax = product.product_tmpl_id.supplier_taxes_id
 
         if purch_tax:
             # Use the default purchase tax set on the product
             defaut_tax = purch_tax[0]
 
             # Get the fiscal position of the supplier
-            supplier= self.pool.get('res.partner').browse(cr, uid, partner_id)
+            supplier = self.pool.get('res.partner').browse(cr, uid, partner_id)
             acc_pos = supplier.property_account_position
 
             # Apply the fiscal position mapping rules
@@ -51,47 +51,54 @@ class hr_expense_line(osv.osv):
 
         return None
 
-
     def create(self, cr, user, vals, context=None):
-        '''
-            Overwrite the create() function to automatically set the default supplier tax
+        """
+            Overwrite the create() function to automatically set the default \
+            supplier tax
             since regular employees cannot access this field.
-        '''
-        vals['tax_id'] = self.get_tax_id(cr, user, vals['product_id'], vals['partner_id'], context)
-        return super(hr_expense_line,self).create(cr, user, vals, context)
+        """
+        vals['tax_id'] = self.get_tax_id(cr, user, vals['product_id'],
+                                         vals['partner_id'], context)
+        return super(hr_expense_line, self).create(cr, user, vals, context)
 
-
-    def onchange_product_id(self, cr, uid, ids, product_id, uom_id, employee_id, context=None):
+    def onchange_product_id(self, cr, uid, ids, product_id, uom_id,
+                            employee_id, context=None):
         if product_id:
-            values  = super(hr_expense_line, self).onchange_product_id(cr, uid, ids, product_id, uom_id, employee_id, context=context)
+            parent_h_e_l = super(hr_expense_line, self)
+            values = parent_h_e_l.onchange_product_id(cr,
+                                                      uid,
+                                                      ids,
+                                                      product_id,
+                                                      uom_id,
+                                                      employee_id,
+                                                      context=context)
         else:
-            values  = {}
+            values = {}
 
-        for id in ids:
-            this    = self.browse(cr, uid, id)
+        for i_id in ids:
+            this = self.browse(cr, uid, i_id)
             if this.name:
-                tax_id  = self.get_tax_id(cr, uid, product_id, this.partner_id.id, context)
+                tax_id = self.get_tax_id(cr, uid, product_id,
+                                         this.partner_id.id, context)
                 values['value'].update({
-                    'tax_id' : tax_id
-                 })
+                    'tax_id': tax_id
+                })
         return values
 
     def onchange_partner_id(self, cr, uid, ids, partner_id, context=None):
-        if partner_id:
-            values = {'value': {}}
-        for id in ids:
-            this    = self.browse(cr, uid, id)
-            tax_id  = self.get_tax_id(cr, uid, this.product_id.id, partner_id, context)
+        values = {'value': {}}
+        for i_id in ids:
+            this = self.browse(cr, uid, i_id)
+            tax_id = self.get_tax_id(cr, uid, this.product_id.id, partner_id,
+                                     context)
             values['value'].update({
-                'tax_id' : tax_id
+                'tax_id': tax_id
             })
         return values
 
     _columns = {
-        'tax_id': fields.many2one('account.tax', 'Tax', domain=[('type_tax_use', '=', 'purchase')]),
-        'partner_id': fields.many2one('res.partner', 'Supplier', required=True),
+        'tax_id': fields.many2one('account.tax', 'Tax',
+                                  domain=[('type_tax_use', '=', 'purchase')]),
+        'partner_id': fields.many2one('res.partner', 'Supplier',
+                                      required=True),
     }
-
-hr_expense_line()
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
